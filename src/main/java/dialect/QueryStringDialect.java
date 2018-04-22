ce96a95c575ce676d28e79567d5d54d61c430ae6
Matt Stewart <smtp.matt@gmail.com>
2018-04-22 14:51:33 +0930

    Added dialect

---
+++ src/main/java/dialect/QueryStringDialect.java
@@ -0,0 +1,17 @@
+package dialect;
+
+import org.thymeleaf.dialect.AbstractDialect;
+import org.thymeleaf.dialect.IExpressionObjectDialect;
+import org.thymeleaf.expression.IExpressionObjectFactory;
+
+public class QueryStringDialect extends AbstractDialect implements IExpressionObjectDialect {
+
+    public QueryStringDialect() {
+        super("queryStringHelper");
+    }
+
+    @Override
+    public IExpressionObjectFactory getExpressionObjectFactory() {
+        return new QueryStringExpressionFactory();
+    }
+}
